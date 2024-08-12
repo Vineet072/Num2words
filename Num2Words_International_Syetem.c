1d81cb24be2c1847074e859642b5ca6ad7f3811e
Vineet072 <128561458+Vineet072@users.noreply.github.com>
2024-08-12 21:17:15 +0530

    Update Num2Words_International_System.c

--- Num2Words_International_Syetem.c
+++ Num2Words_International_Syetem.c
@@ -55,10 +55,10 @@ int main() {
     else if (f>19) {
         if (f%10==0) {
             strcpy(d[flag++],b[(f/10)-2]);
-            strcpy(d[flag++],a[-1+f%10]);
             flag2++;
         }
         else if(f%10>0) {
+            
             strcpy(d[flag++],b[(f/10)-2]);
             strcpy(d[flag++],a[-1+f%10]);
             flag2++;
@@ -71,7 +71,7 @@ int main() {
     //Thousand
     f=(e%1000000)/1000;
     if ((f%1000)/100>0) {
-        strcpy(d[flag++],a[-1+(e%1000)/100]);
+        strcpy(d[flag++],a[-1+f/100]);
         strcpy(d[flag++],c[0]);
         if ((e%100000)/1000==0) {
             strcpy(d[flag++],c[1]);
@@ -115,8 +115,25 @@ int main() {
         }
     }
 
+    int total_length=0;
+
     for(int i=0;i<flag;i++) {
-        printf("%s ",d[i]);
+        total_length+=strlen(d[i])+1;
     }
 
+    char *finalresult=(char *)malloc(total_length*sizeof(char));
+    if (finalresult==NULL) {
+        return "Zero";
+    }
+
+    finalresult[0]='\0';
+
+    for(int i=0;i<flag;i++) {
+        strcat(finalresult,d[i]);
+        if (i<flag-1) {
+            strcat(finalresult," ");
+        }
+    }
+
+    printf("%s",finalresult);
 }
